1a11778e0a69dd6f2b2b335a6f47da12fefe0bd8
sin013 <158771236+sin013@users.noreply.github.com>
2024-07-01 23:01:13 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = 2, True, 'кряква'
+print("Immutable tuple:",immutable_var)
+#immutable_var[0]=3
+#print(immutable_var)
+mutable_list = [2, False, 'черополох']
+mutable_list[0] = 2
+mutable_list.append(False)
+print('Mutable list:',mutable_list)
